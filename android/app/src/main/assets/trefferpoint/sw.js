524ceff5716313fcaa95038878ef0f7992bd57d8
bmcservice <xerato@email.de>
2026-04-27 10:02:20 +0200

    v2.3.84: index.html loop() akzeptiert inputMode='rtsp' (war Display-Bug)

--- android/app/src/main/assets/trefferpoint/sw.js
+++ android/app/src/main/assets/trefferpoint/sw.js
@@ -1,6 +1,6 @@
 ﻿// TrefferPoint Service Worker
 // CACHE_VER mit APP_VERSION in index.html zusammen bumpen
-const CACHE_VER = 'tp-2.3.83';
+const CACHE_VER = 'tp-2.3.84';
 const PRECACHE = ['./index.html', './manifest.json', './version.json'];
 
 self.addEventListener('install', e => {
