4cdaadf78db20bac555ea71ceadc76e0e8bf64ff
bmcservice <xerato@email.de>
2026-04-27 22:04:49 +0200

    v2.3.110: Mask-Position via Slider statt Drag (zuverlässig in WebView)

--- android/app/src/main/assets/trefferpoint/sw.js
+++ android/app/src/main/assets/trefferpoint/sw.js
@@ -1,6 +1,6 @@
 ﻿// TrefferPoint Service Worker
 // CACHE_VER mit APP_VERSION in index.html zusammen bumpen
-const CACHE_VER = 'tp-2.3.109';
+const CACHE_VER = 'tp-2.3.110';
 const PRECACHE = ['./index.html', './manifest.json', './version.json'];
 
 self.addEventListener('install', e => {
